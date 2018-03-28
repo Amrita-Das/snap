6b3acb2885c737f1db61c4db661750cfb28b8f16
Amrita-Das <37376787+Amrita-Das@users.noreply.github.com>
2018-03-28 18:31:42 +0530

    Add files via upload

--- edits.py
+++ edits.py
@@ -6,14 +6,22 @@ Created on Sat Mar 24 12:05:58 2018
 """
 import requests
 import json
-from elasticsearch import Elasticsearch
+from elasticsearch import Elasticsearch,helpers
 
 es= Elasticsearch([{'host': 'localhost', 'port': 9200}])
 
 r = requests.get('http://localhost:9200') 
-i = 1
-while r.status_code == 200:
-    r = open('snapdeal.json', 'r')
-    es.index(index='products',doc_type='snapdeal',id=i,body=json.load(r))
-    i=i+1
-print(i)
+
+def push(fp):
+    
+      doc=  {"_index":"products",
+    "_type":"snapdeal",
+    "_source":json.loads(fp.read())
+    }
+      yield(doc)
+fp = open("snapdeal.json",'r')
+    #es.create(index='products',doc_type='snapdeal',id=i,body=None)
+helpers.bulk(actions=push(fp),client=es)
+    
+res = es.search(index="products", body={"query": {"match_all": {}}})
+print("Got %d Hits:" % res['hits']['total'])
